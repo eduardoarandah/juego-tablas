f3825be64149989661f33ec4533943fffb1cb839
Eduardo Aranda <eduardoarandah@gmail.com>
2025-09-06 08:57:20 -0600

    quitar el mensaje de has ganado

--- src/App.tsx
+++ src/App.tsx
@@ -168,22 +168,6 @@ function App() {
             Gana todos los animalitos
           </h1>
 
-          {/* si tenemos todos los animalitos, mostrar el unicornio */}
-          {Object.keys(progress).length === 12 && (
-            <div className="text-center p-4 bg-gradient-to-b from-blue-100 to-green-100 my-12 rounded-lg shadow-lg">
-              <div className="text-3xl font-bold mb-8 text-purple-700">
-                Felicidades, has ganado al unicornio 😀
-              </div>
-              <div className="text-8xl mb-8">🦄</div>
-              <button
-                onClick={resetGame}
-                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded-lg text-lg"
-              >
-                Reiniciar el juego
-              </button>
-            </div>
-          )}
-
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8 justify-items-center">
             {Array.from({ length: 12 }, (_, i) => i + 1).map((tableNumber) => (
               <button
